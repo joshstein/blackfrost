d185f5df87c3952e802ece311da6ec46245f42ed
Joshua Stein <josh.t.stein@gmail.com>
2013-11-23 05:52:23 -0600

    Continued implementation of UI pages.

---
+++ Blackfrost/Views/PvPClassicPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Blackfrost.Views
+{
+    /// <summary>
+    /// Interaction logic for PvPClassicPage.xaml
+    /// </summary>
+    public partial class PvPClassicPage : UserControl
+    {
+        public PvPClassicPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
